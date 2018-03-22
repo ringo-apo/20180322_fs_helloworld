509b2e2093945e9cb2b1e47498c516f9e76eb4b4
ringo-apo <ringo1625@gmail.com>
2018-03-22 20:03:44 +0900

    201803222009

---
+++ ConsoleApplication1/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+// F# の詳細については、http://fsharp.org を参照してください
+// 詳細については、'F# チュートリアル' プロジェクトを参照してください。
+
+[<EntryPoint>]
+let main argv = 
+
+    Console.WriteLine "Hello World"
+    0
